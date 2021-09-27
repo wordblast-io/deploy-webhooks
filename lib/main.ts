33be604404fb71d5ed68ad0b4bf30b1993e143d7
Emilio Barradas <decalerss@gmail.com>
2021-09-27 01:22:56 -0400

    Verbose hook

--- lib/main.ts
+++ lib/main.ts
@@ -5,6 +5,17 @@ import { exec } from "child_process";
 dotenv.config();
 
 hooks()
-    .hook('redeployment', () => exec(`scripts/redeploy.sh ${process.env.REGISTRY_TOKEN}`))
+    .hook('redeployment', () =>
+        exec(
+            `scripts/redeploy.sh ${process.env.REGISTRY_TOKEN}`,
+            (err, stdout, stderr) => {
+                if (err !== undefined) {
+                    console.error('error', err);
+                    return;
+                }
+                console.log('stdout', stdout);
+                console.log('stderr', stderr);
+            })
+        )
     .token(process.env.WEBHOOK_TOKEN)
     .listen(process.env.LISTEN_PORT);
