06e615861da634fb598d1abff765377df8c83c20
Emilio Barradas <decalerss@gmail.com>
2021-09-27 01:14:15 -0400

    Pass registry token to deployment script.

--- lib/main.ts
+++ lib/main.ts
@@ -5,6 +5,6 @@ import { exec } from "child_process";
 dotenv.config();
 
 hooks()
-    .hook('redeployment', () => exec('scripts/redeploy.sh'))
+    .hook('redeployment', () => exec(`scripts/redeploy.sh ${process.env.REGISTRY_TOKEN}`))
     .token(process.env.WEBHOOK_TOKEN)
     .listen(process.env.LISTEN_PORT);
--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,1 +1,2 @@
 # Redeploy docker container
+echo "$0 $1 $2"
