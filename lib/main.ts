40f047fb42c181dcfa5b3e930adab505e8bb8542
Emilio Barradas <decalerss@gmail.com>
2021-09-27 01:27:01 -0400

    Verbose hook

--- lib/main.ts
+++ lib/main.ts
@@ -9,12 +9,9 @@ hooks()
         exec(
             `scripts/redeploy.sh ${process.env.REGISTRY_TOKEN}`,
             (err, stdout, stderr) => {
-                if (err !== undefined) {
-                    console.error('error', err);
-                    return;
-                }
-                console.log('stdout', stdout);
-                console.log('stderr', stderr);
+                if (err !== null) console.error('error', err);
+                if (stdout !== undefined) console.log('stdout', stdout);
+                if (stderr !== undefined) console.log('stderr', stderr);
             })
         )
     .token(process.env.WEBHOOK_TOKEN)
